5398d875024ecad03e5f5ad4949b607ee32d8393
Juan <juantvaldez85.@gmail.com>
2021-08-19 22:17:00 +0000

    Cats as a Service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,20 @@
+const args = process.argv;
+const array = args.slice(2, args.length);
 const request = require("request");
 
+// const name = array;
+console.log(array[0]);
 request(
-  "https://api.thecatapi.com/v1/breeds/search?q=sib",
+  `https://api.thecatapi.com/v1/breeds/search?q=${array[0]}`,
   (error, response, body) => {
     const data = JSON.parse(body);
 
     console.log("error:", error);
     console.log("statusCode:", response && response.status);
-    console.log("The Data is: \n", data[0].description);
+    console.log(
+      "\n                   ********   Cat Description   ********\n \n",
+      data[0].description + "\n"
+    );
     // console.log(typeof data);
   }
 );
